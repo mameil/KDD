00c4af24fe00167c06737e22e86bd0cdea4cf4bc
kyu9 <kyudo97@naver.com>
2022-05-19 22:19:17 +0900

    [KDD] connect to docker's mysql

--- src/main/java/me/kdshim/kdd_j/link/Link.java
+++ src/main/java/me/kdshim/kdd_j/link/Link.java
@@ -1,8 +1,16 @@
 package me.kdshim.kdd_j.link;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Link {
+    @Id
+    private String url;
     private String category;
     private String name;
-    private String url;
     private String desc;
 }
