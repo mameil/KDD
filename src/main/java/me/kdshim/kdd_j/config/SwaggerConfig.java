0ceff8d464294c98163681acaefed84baeba1c3e
kyu9 <kyudo97@naver.com>
2022-06-05 13:53:27 +0900

    [KDD] Swagger 정리 완료

--- src/main/java/me/kdshim/kdd_j/config/SwaggerConfig.java
+++ src/main/java/me/kdshim/kdd_j/config/SwaggerConfig.java
@@ -5,33 +5,41 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.lang.reflect.Parameter;
 
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("build/swagger/codegen/api"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-
-//    public ApiInfo apiInfo(){
-//        return new ApiInfoBuilder()
-//                .title("KDD's API")
-//                .version("0.0.1")
-//                .description("Swagger for KDD")
-//                .build();
-//    }
-
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui")
-//                .addResourceLocations("/spec/kdd.yaml");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any()) // 특정 패키지경로를 API문서화 한다. 1차 필터
+                .paths(PathSelectors.any()) // apis중에서 특정 path조건 API만 문서화 하는 2차 필터
+                .build()
+                .groupName("API 1.0.0") // group별 명칭을 주어야 한다.
+                .pathMapping("/")
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false); // 400,404,500 .. 표기를 ui에서 삭제한다.
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("KDD's Swagger")
+                .description("KDD의 스웨거")
+                .version("1.0.0")
+                .termsOfServiceUrl("")
+//                .contact()
+                .license("")
+                .licenseUrl("")
+                .build()
+                ;
+    }
 }
