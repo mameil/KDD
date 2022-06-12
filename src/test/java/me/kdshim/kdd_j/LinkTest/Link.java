1944d1136fca76405907cc0d70abe0547a6431ca
kyu9 <kyudo97@naver.com>
2022-06-13 08:34:35 +0900

    테스트환경 확인

--- src/test/java/me/kdshim/kdd_j/LinkTest/Link.java
+++ src/test/java/me/kdshim/kdd_j/LinkTest/Link.java
@@ -3,21 +3,25 @@ package me.kdshim.kdd_j.LinkTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
-public class Link {
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class Link throws Exception{
 
     @Autowired
     MockMvc mvc;
 
     @Test
-    public void checkMock() throws Exception {
+    public void checkMock(){
         mvc.perform(MockMvcRequestBuilders.get("/links"))
                 .andExpect(MockMvcResultMatchers.status().is(200));
     }
