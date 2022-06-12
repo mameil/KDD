2c20c37e221a299057147a36159f4b08aebdd45e
kyu9 <kyudo97@naver.com>
2022-06-12 15:16:20 +0900

    [KDD] show sql에서 파라미터 보이도록 설정

--- src/main/java/me/kdshim/kdd_j/book/BookRepository.java
+++ src/main/java/me/kdshim/kdd_j/book/BookRepository.java
@@ -2,6 +2,12 @@ package me.kdshim.kdd_j.book;
 
 import me.kdshim.kdd_j.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
 public interface BookRepository extends JpaRepository<Book, String> {
+
 }
