3fe3d559831936212aecf11927311f4e664154f4
kyu9 <kyudo97@naver.com>
2022-07-01 22:53:07 +0900

    [EffectiveJava] second_item example #2

--- src/main/java/me/kdshim/kdd_j/checking/Teeth.java
+++ src/main/java/me/kdshim/kdd_j/checking/Teeth.java
@@ -1,8 +1,12 @@
 package me.kdshim.kdd_j.checking;
 
+import lombok.Builder;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
+@Builder
+@ToString
 public class Teeth {
     String first;
     String second;
@@ -44,15 +48,34 @@ public class Teeth {
         this.sixth = sixth;
     }
 
+    public Teeth(String... tooth){
+        System.out.println(tooth);
+    }
+
     //JavaBeans Pattern
-//    public static void main(String[] args) {
-//        Teeth teeth = new Teeth();
-//        teeth.setFirst("THIS IS FIRST");
-//        teeth.setSecond("THIS IS SECOND");
-//        teeth.setThird("THIS IS THIRD");
-//        teeth.setFourth("THIS IS FOURTH");
-//        teeth.setFifth("THIS IS FIFTH");
-//
-//    }
+    public static void main(String[] args) {
+        Teeth teeth = new Teeth();
+        teeth.setFirst("THIS IS FIRST");
+        teeth.setSecond("THIS IS SECOND");
+        teeth.setThird("THIS IS THIRD");
+        teeth.setFourth("THIS IS FOURTH");
+        teeth.setFifth("THIS IS FIFTH");
+
+        Teeth teeth2 = new Teeth("THIS IS FIRST", "THIS IS SECOND", "THIS IS THIRD", "THIS IS FOURTH", "THIS IS FIFITH");
+
+        Teeth teeth3 = Teeth.builder()
+                .first("THIS IS FIRST")
+                .second("THIS IS SECOND")
+                .third("THIS IS THIRD")
+                .fourth("THIS IS FOURTH")
+                .fifth("THIS IS FIFTH")
+                .build();
+
+        Teeth teeth4 = new Teeth("FIR");
+        Teeth teeth5 = new Teeth("FIR", "SEC");
+
+        System.out.println("first : " + teeth4.toString());
+        System.out.println("sec : " + teeth5.toString());
+    }
 }
 
