028b857bc570cfaf7c5f06c1a677173f167a9096
kyu9 <kyudo97@naver.com>
2022-09-04 18:05:02 +0900

    [KDD] 멤버 조회 테스트코드 작성

--- src/main/java/me/kdshim/kdd_j/view/dto/ForgetDto.java
+++ src/main/java/me/kdshim/kdd_j/view/dto/ForgetDto.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class ForgetDto {
-    String id;
     String userName;
     String birthDateYYYYMMDD;
 }
--- src/test/java/me/kdshim/kdd_j/MemberTest/MemberTest.java
+++ src/test/java/me/kdshim/kdd_j/MemberTest/MemberTest.java
@@ -1,6 +1,7 @@
 package me.kdshim.kdd_j.MemberTest;
 
 import lombok.SneakyThrows;
+import me.kdshim.kdd_j.common.KDDError;
 import me.kdshim.kdd_j.config.KDTest;
 import me.kdshim.kdd_j.config.MyMockMvc;
 import me.kdshim.kdd_j.member.Member;
@@ -60,5 +61,69 @@ public class MemberTest extends MyMockMvc {
         Member dbM = memberRepository.findById("kdshim").get();
 
         Assertions.assertEquals(testM.getLoginId(), dbM.getLoginId());
+    }
+
+    @KDTest
+    public void memberFindTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/member/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "  \"loginId\": \"kdshim\",\n" +
+                                "  \"password\": \"rbeh!234\",\n" +
+                                "  \"role\": \"ADMIN\",\n" +
+                                "  \"name\": \"심규도\",\n" +
+                                "  \"birthDateYYYYMMDD\": \"19970329\",\n" +
+                                "  \"married\": false,\n" +
+                                "  \"email\": \"kyudo97@naver.com\",\n" +
+                                "  \"oldAddr\": \"경기도 고양시 일산서구 강선길 42\",\n" +
+                                "  \"addr\": \"경기도 고양시 일산서구 강선로 30\",\n" +
+                                "  \"addrDetail\": \"1501동\",\n" +
+                                "  \"zipCode\": \"10365\"\n" +
+                                "}")
+                )
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.loginId").value("kdshim"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password").value("rbeh!234"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.role").value(GetMemberDto.RoleEnum.ADMIN.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.birthDateYYYYMMDD").value("19970329"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.married").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("kyudo97@naver.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.oldAddr").value("경기도 고양시 일산서구 강선길 42"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.addr").value("경기도 고양시 일산서구 강선로 30"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.addrDetail").value("1501동"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("10365"))
+        ;
+
+        mvc.perform(MockMvcRequestBuilders.post("/login/find/member")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"userName\": \"심규도\",\n" +
+                        "  \"birthDateYYYYMMDD\": \"19970329\"\n" +
+                        "}")
+        )
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.loginId").value("kdshim"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password").value("rbeh!234"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.role").value(GetMemberDto.RoleEnum.ADMIN.toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.birthDateYYYYMMDD").value("19970329"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.married").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("kyudo97@naver.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.address.oldAddr").value("경기도 고양시 일산서구 강선길 42"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.address.addr").value("경기도 고양시 일산서구 강선로 30"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.address.addrDetail").value("1501동"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.address.zipCode").value("10365"))
+        ;
+
+        mvc.perform(MockMvcRequestBuilders.post("/login/find/member")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "  \"userName\": \"실패테스트\",\n" +
+                                "  \"birthDateYYYYMMDD\": \"19970329\"\n" +
+                                "}")
+                )
+                .andExpect(MockMvcResultMatchers.status().is(400))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(KDDError.MEMBER_NOT_FOUND.getCode()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(KDDError.MEMBER_NOT_FOUND.getMsg()))
+        ;
     }
 }
