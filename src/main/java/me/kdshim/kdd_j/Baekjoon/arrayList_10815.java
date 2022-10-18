59105b9392c7abc7009b087634007dbfc2ed3f3f
kyu9 <kyudo97@naver.com>
2022-10-18 21:57:38 +0900

    [Baekjoon] 배열과 맵 - 10815 #2

--- src/main/java/me/kdshim/kdd_j/Baekjoon/arrayList_10815.java
+++ src/main/java/me/kdshim/kdd_j/Baekjoon/arrayList_10815.java
@@ -3,36 +3,60 @@ package me.kdshim.kdd_j.Baekjoon;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class arrayList_10815 {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int mainCnt = Integer.parseInt(br.readLine());
-        HashSet<Integer> mainSet = new HashSet<>();
+        int[] mainArr = new int[mainCnt];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0 ; i<mainCnt; i++){
-            mainSet.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < mainCnt; i++) {
+            mainArr[i] = Integer.parseInt(st.nextToken());
         }
 
         int comCnt = Integer.parseInt(br.readLine());
         int[] comArr = new int[comCnt];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<comCnt; i++){
+        for (int i = 0; i < comCnt; i++) {
             comArr[i] = Integer.parseInt(st.nextToken());
         }
 
         int[] result = new int[comCnt];
-        for(int i=0; i<comCnt; i++){
-            result[i] = mainSet.contains(comArr[i]) ? 1 : 0;
+
+        Arrays.sort(mainArr);
+
+        for(int i=0; i<comArr.length; i++){
+            result[i] = Arrays.binarySearch(mainArr, comArr[i]) >= 0 ? 1 : 0;
         }
 
-        for(int i=0; i<comCnt; i++){
+        for (int i = 0; i < comCnt; i++) {
             System.out.print(result[i] + " ");
         }
+    }
+
+    public static int binarySearch(int[] arr, int index) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while (end - start >= 0) {
+            mid = (end + start) / 2;
+
+            if (arr[mid] == index) {
+                return 1;
+            } else if (arr[mid] < index) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return 0;
     }
 }
