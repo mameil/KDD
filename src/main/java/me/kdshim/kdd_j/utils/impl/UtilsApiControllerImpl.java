30932830faeb965571821d9e511474d9d63cbff5
kyu9 <kyudo97@naver.com>
2022-12-24 00:59:32 +0900

    [KDD] spec 정의 implement 까지 추가 완료

--- src/main/java/me/kdshim/kdd_j/utils/impl/UtilsApiControllerImpl.java
+++ src/main/java/me/kdshim/kdd_j/utils/impl/UtilsApiControllerImpl.java
@@ -2,7 +2,16 @@ package me.kdshim.kdd_j.utils.impl;
 
 import me.kdshim.kdd_j.swagger.api.UtilsApiDelegate;
 import me.kdshim.kdd_j.swagger.model.GetLinkDto;
+import me.kdshim.kdd_j.swagger.model.StringDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UtilsApiControllerImpl implements UtilsApiDelegate {
+    //di service
+
+    @Override
+    public ResponseEntity<StringDto> toStringToJSON(StringDto body) {
+        return UtilsApiDelegate.super.toStringToJSON(body);
+    }
 }
