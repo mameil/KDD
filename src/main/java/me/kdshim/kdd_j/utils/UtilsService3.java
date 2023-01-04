0435a728b72a1c3b0a73a32aaeceb0d809b8ed0d
kyu9 <kyudo97@naver.com>
2023-01-04 22:51:53 +0900

    [KDD] json parsing - inner object 이름 제거중

--- src/main/java/me/kdshim/kdd_j/utils/UtilsService3.java
+++ src/main/java/me/kdshim/kdd_j/utils/UtilsService3.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 @Log4j2
@@ -38,8 +39,11 @@ public class UtilsService3 {
             log.info("substring1 : {}", substring1);
 
             str = substring + substring1;
-
         }
+
+        if(str.charAt(str.length()-2) == ')')
+
+        log.info("LAST PARSE : {}", str);
 
         //기본적인 파싱 작업
         //Request processing failed; nested exception is java.util.regex.PatternSyntaxException: Unclosed group near index 1
@@ -59,6 +63,39 @@ public class UtilsService3 {
         //배열인 경우에는 [] 밖에 "" 제거해주어야함
         str = str.replaceAll("\"\\[", "[");
         str = str.replaceAll("\\]\"", "]");
+
+        //가장 처음에 있는 {을 재치고 그 다음부터 { 을 찾고 만약에 그 전에 :으로 시작할 수 있게 처리하자
+        int index = str.indexOf("{");
+        List<Integer> list = new ArrayList<>();
+        while (index >= 0) {
+            index = str.indexOf("{", index + 1);
+            if(index > -1){
+                String substring = str.substring(index - 10, index + 10);
+                System.out.println(str.substring(0, index) + "\"" + str.substring(index, str.length()));
+            }
+            list.add(index);
+        }
+
+        for (Integer idx : list) {
+            if(idx != -1) {
+                //여기 breakpoint
+                int start = idx;
+                while (str.charAt(idx - 1) != ':' && str.charAt(idx - 1) != '[') {
+                    idx--;
+                }
+                //여기 breakpoint
+                String substring = str.substring(0, idx);
+                String substring1 = str.substring(start, str.length());
+                System.out.println("==============================================");
+                System.out.println(substring);
+                System.out.println(substring.substring(substring.length() - 10, substring.length()));
+                System.out.println("==============================================");
+                System.out.println(substring1);
+                System.out.println(substring1.substring(0, 10));
+                System.out.println("==============================================");
+                str = substring + substring1;
+            }
+        }
 
         log.info("parsed STR : {}", str);
 
